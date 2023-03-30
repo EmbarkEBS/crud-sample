fbe2d2d31ff8341bc0935ef71da384dd73623e92
embarkdeveloper <91586071+EmbarkEBS@users.noreply.github.com>
2023-03-30 16:05:49 +0530

    Files added

---
+++ dbconnection.php
@@ -0,0 +1,8 @@
+<?php
+<<<<<<< HEAD
+$con=mysqli_connect("localhost", "techdemy_crud", "techdemypwd", "techdemy_crud");
+if(mysqli_connect_errno())
+{
+echo "Connection Failed in server".mysqli_connect_error();
+}
+?>
